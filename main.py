d882f1fed8be9e0fab036076599e1b427acdb400
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-25 17:35:42 -0500

    None

--- main.py
+++ main.py
@@ -348,3 +348,22 @@
 # join
 # sentence = ' '.join(['hi', 'my', 'name', 'is', 'Gael'])
 # print(sentence)
+
+# LIST UNPACKING
+# a,b,c = [1,2,3]
+#
+# print(a)
+# print(b)
+# print(c)
+
+# a,b,c, *other, d = [1,2,3,4,5,6,7,8,9]
+#
+# print(a)
+# print(b)
+# print(c)
+# print(other)
+# print(d)
+
+# None (null in JavaScript)
+cookies = None
+print(cookies)

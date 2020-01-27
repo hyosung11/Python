d8c281b6e8ddfc97f6a7f76e125ed8f5ced07db9
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-27 12:58:29 -0500

    pets everywhere

---
+++ pets.py
@@ -0,0 +1,45 @@
+class Pets():
+    animals = []
+
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#1 Add another Cat
+class SungOh(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = [Simon('Simon', 4), Sally('Sally', 21), SungOh('SungOh', 4)]
+
+#3 Instantiate the Pet class with all your cats use variable my_pets
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
+

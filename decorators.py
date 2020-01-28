f92bc283e45c4e4b5701d2df34a431dff7a832c7
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-28 13:26:40 -0500

    time performance decorator

--- decorators.py
+++ decorators.py
@@ -41,17 +41,40 @@
 
 
 # Decorator 3 Exercise
-def my_decorator(func):
-    def wrap_func(*args, **kwargs):
-        print('********************************')
-        func(*args, **kwargs)
-        print('********************************')
-    return wrap_func
+# def my_decorator(func):
+#     def wrap_func(*args, **kwargs):
+#         print('********************************')
+#         func(*args, **kwargs)
+#         print('********************************')
+#     return wrap_func
 
-@my_decorator
-def hello(greeting, emoji=':('):
-    print(greeting, emoji)
+# @my_decorator
+# def hello(greeting, emoji=':('):
+#     print(greeting, emoji)
 
 
-# same as => hello = my_decorator(hello)
-hello('gotcha')
+# # same as => hello = my_decorator(hello)
+# hello('gotcha')
+
+
+#performance decorator.
+from time import time
+
+
+def performance(fn):
+  def wrapper(*args, **kwargs):
+    t1 = time()
+    result = fn(*args, **kwargs)
+    t2 = time()
+    print(f'took {t2-t1} seconds')
+    return result
+  return wrapper
+
+
+@performance
+def long_time():
+    for i in range(10000):
+        i*5
+
+
+long_time()

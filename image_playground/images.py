4d0a6304ba6d49788555234029cd00d082571972
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-30 12:35:47 -0500

    manipulate astro.jpg with reduce and thumbnail

--- image_playground/images.py
+++ image_playground/images.py
@@ -1,10 +1,10 @@
-from PIL import Image, ImageFilter
+# from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
+# img = Image.open('./Pokedex/pikachu.jpg')
 # filtered_img = img.filter(ImageFilter.BLUR)
 # filtered_img = img.filter(ImageFilter.SMOOTH)
 # filtered_img = img.filter(ImageFilter.SHARPEN)
-filtered_img = img.convert('L')
+# filtered_img = img.convert('L')
 # filtered_img.save('grey.png', 'png')
 # filtered_img.show()
 # filtered_img.rotate(90)
@@ -14,12 +14,28 @@ filtered_img = img.convert('L')
 # resize.save('grey.png', 'png')
 
 # crop image
-box = (100, 100, 400, 400)
-region = filtered_img.crop(box)
-region.save('grey.png', 'png')
+# box = (100, 100, 400, 400)
+# region = filtered_img.crop(box)
+# region.save('grey.png', 'png')
 
 # print(img)
 # print(img.format)
 # print(img.size)
 # print(img.mode)
 # print(dir(img))
+
+
+# =================================
+from PIL import Image, ImageFilter
+
+img = Image.open('./astro.jpg')
+# print(img.size)
+
+# doesn't keep aspect ratio
+# reduce_img = img.resize((400, 400))
+# reduce_img.save('thumbnail.jpg')
+
+# thumbnail - keeps the aspect ratio
+img.thumbnail((400, 400))
+img.save('thumbnail.jpg')
+print(img.size)

c7af7a1e45bdcd02cc199dab5df9773066415503
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-31 15:51:19 -0500

    remove components page and links

--- web_server/server.py
+++ web_server/server.py
@@ -3,33 +3,38 @@ app = Flask(__name__)
 print(__name__)
 
 # decorator
-@app.route('/index.html')
-def my_home(username=None, post_id=None):
+@app.route('/')
+def my_home():
     return render_template('index.html')
 
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
 
 
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/work.html')
-def work():
-    return render_template('work.html')
+# @app.route('/components.html')
+# def components():
+#     return render_template('components.html')
 
 
-@app.route('/works.html')
-def works():
-    return render_template('works.html')
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/work.html')
+# def work():
+#     return render_template('work.html')
+
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
 
 
 
